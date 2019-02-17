212cd7ca181c143addb354448e64167b8eee5272
keoma <keoma.brun@inria.fr>
2019-02-17 22:25:04 +0100

    Fix #47

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,39 +7,3 @@
 pkg_dependencies="php5-fpm sipcalc hostapd iptables iw dnsmasq"
 nonfree_packages="firmware-linux-free firmware-linux-nonfree firmware-atheros firmware-realtek firmware-ralink firmware-libertas atmel-firmware zd1211-firmware"
 free_packages="firmware-linux-free"
-
-#
-# Helper to start/stop/.. a systemd service from a yunohost context,
-# *and* the systemd service itself needs to be able to run yunohost
-# commands.
-#
-# Hence the need to release the lock during the operation
-#
-# usage : ynh_systemctl yolo restart
-#
-function ynh_systemctl()
-{
-  local ACTION="$1"
-  local SERVICE="$2"
-  local LOCKFILE="/var/run/moulinette_yunohost.lock"
-
-  # Launch the action
-  systemctl "$ACTION" "$SERVICE" &
-  local SYSCTLACTION=$!
-
-  # Save and release the lock...
-  cp $LOCKFILE $LOCKFILE.bkp.$$
-  ynh_secure_remove $LOCKFILE
-
-  # Wait for the end of the action
-  wait $SYSCTLACTION
-
-  # Make sure the lock is released...
-  while [ -f $LOCKFILE ]
-  do
-    sleep 0.1
-  done
-
-  # Restore the old lock
-  mv $LOCKFILE.bkp.$$ $LOCKFILE
-}
