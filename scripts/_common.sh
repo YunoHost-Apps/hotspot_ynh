7803699e667d654bc9979b8f83a59fcdc9a2eee0
HgO <hgo@batato.be>
2023-08-23 10:24:43 +0200

    display unused wifi devices

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,7 +14,27 @@ free_firmware_packages="firmware-ath9k-htc"
 
 function iw_devices()
 {
-  echo -n $(/sbin/iw dev | grep Interface | grep -v 'mon\.' | grep -v hotspot | awk '{ print $NF }') | tr ' ' '|'
+  /sbin/iw dev | grep Interface | grep -v 'mon\.' | grep -v hotspot | awk '{ print $NF }'
+}
+
+function used_iw_devices()
+{
+  local app_shortname="${app%%__*}"
+  local hotspot_apps=$(yunohost app list --output-as json | jq -r .apps[].id | grep -F $app_shortname)
+  # Remove this app from hotspot apps list
+  local other_hotspot_apps=$(grep -F -x -v $app <<< ${hotspot_apps})
+  for hotspot_app in ${other_hotspot_apps}; do
+    hotspot_wifi_device=$(ynh_app_setting_get --app=$hotspot_app --key=wifi_device)
+    if [[ -n ${hotspot_wifi_device} ]]; then
+      echo "${hotspot_wifi_device}"
+    fi
+  done
+}
+
+function unused_iw_devices()
+{
+  # Only prints devices that are not in the list of used devices
+  iw_devices | grep -F -v -f <(used_iw_devices)
 }
 
 function check_armbian_nonfree_conflict()
